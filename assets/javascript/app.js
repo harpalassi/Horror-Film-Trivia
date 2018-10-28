3e0dd4156c238c79858126224eaac4e4f73bdcca
Speedway <harpalassi@gmail.com>
2018-10-28 15:45:17 -0500

    added q/a html

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+
+
+//questions
+
+var questions = [{
+    question: "Which Halloween film did not include Michael Myers?",
+    answers: ["Halloween 4", "Halloween 3", "Halloween 6", "Halloween H20"],
+    correctAnswer: "Halloween 3"
+}, {
+    question: "Who was the killer in Friday the 13th part 1?",
+    answers: ["Jason", "Jason's Mother", "Placeholder", "Another Placeholder"],
+    correctAnswer: "Jason's Mother"
+    
+} ]
+
+console.log(answers);
